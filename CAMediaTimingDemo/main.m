33a10de76f2d7723edec3bca7701ef14cb8218d8
卢强 <qiang.lu@renren-inc.com>
2016-05-09 14:17:50 +0800

    Initial Commit

---
+++ CAMediaTimingDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CAMediaTimingDemo
+//
+//  Created by renren on 16/5/9.
+//  Copyright © 2016年 lasso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
